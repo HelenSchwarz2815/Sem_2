6713b883959e603fa083aca751d3a677c93ba6b8
Elena_Parvatkina <e.parvatkina2815@gmail.com>
2022-12-16 21:54:04 +0300

    new

--- Sem_2/Program.cs
+++ Sem_2/Program.cs
@@ -1,2 +1,45 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задача 10: Напишите программу, которая принимает 
+//на вход трёхзначное число и на выходе показывает вторую 
+//цифру этого числа. Через строку решать нельзя.
+
+//Console.WriteLine("введите трехзначное число ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int x1 = num/100;
+//int x2 = (num - (x1 * 100)) / 10;
+//int x3 = num % 10;
+//if (num>99 && num<=999) Console.WriteLine($"вторая цифра числа {x2} ");
+//else Console.WriteLine("введено некорректное число");
+
+//Задача 13: Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+//Console.WriteLine("введите число ");
+//int num = Convert.ToInt32(Console.ReadLine());
+//int i = num;
+//if (num<100) Console.WriteLine("введено некорректное число ");
+//while (i>999)
+//{
+//i = (i / 10);
+//} 
+//if (i>99 && i<=999)
+//{
+//i = i % 10;
+//Console.WriteLine($"третья цифра введенного числа {i} ");
+//}
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.  
+ 
+//Console.WriteLine("введите число ");
+//int num = Convert.ToInt32(Console.ReadLine()); 
+//if (num==1) Console.WriteLine("понедельник ");
+//if (num==2) Console.WriteLine("вторник ");
+//if (num==3) Console.WriteLine("среда ");
+//if (num==4) Console.WriteLine("четверг ");
+//if (num==5) Console.WriteLine("пятница ");
+//if (num==6) Console.WriteLine("суббота ");
+//if (num==7) Console.WriteLine("воскресенье ");
+//if (num<=5) Console.WriteLine($"Печаль вообще! Это не выходной день ");
+//if (num>7) Console.WriteLine($"Такого дня недели не существует! ");
+//if (num>5 && num<8) Console.WriteLine($"выходной ");
+
+
